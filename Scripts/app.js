3b325d22b0d2317e4910daea8e9e965731aaec2a
Khalil Frough <khalilfrough@Khalils-MBP.gateway>
2019-01-05 19:28:34 +1100

    added JavaScript file and made an IIFE

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,12 @@
 //this is a custom JavaScript file
+
+/* JavaScript has a function called IIFE => Immediately Invocked Function Expression */
+
+(function(){
+    function start(){
+        console.log("App started!");
+        console.log("--------------------");
+        console.log("Hello, World");
+    }
+    window.onload=start();
+})()
