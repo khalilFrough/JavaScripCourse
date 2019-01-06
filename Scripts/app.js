ee792b26c7f06da28a4ab50bf31d9f1cee84de6e
Khalil Frough <khalilfrough@Khalils-MBP.gateway>
2019-01-06 15:54:08 +1100

    Added an about page to my file

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,12 +1,12 @@
 //this is a custom JavaScript file
-
 /* JavaScript has a function called IIFE => Immediately Invocked Function Expression */
 
 (function(){
     function start(){
+        let title=document.title;
         console.log("App started!");
         console.log("--------------------");
-        console.log("Hello, World");
+        console.log(`%cTitle: ${title}`,`font-size:15px; color: red;`);
     }
     window.onload=start();
 })()
