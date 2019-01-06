de68d36cd01d68e002bd94cfc42772720a4b9a92
Khalil Frough <khalilfrough@Khalils-MBP.gateway>
2019-01-06 22:02:50 +1100

    added some codes in the app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,11 +2,31 @@
 /* JavaScript has a function called IIFE => Immediately Invocked Function Expression */
 
 (function(){
+    // this is used to prevent JavaScript to automatically make variables 
+    "use strict"
+
     function start(){
+
         let title=document.title;
+
+        switch(title){
+            case "Home":
+                let AboutButton=document.getElementById("AboutButton");
+                AboutButton.addEventListener("click",function(){
+                console.log("About Button was clicked!");
+                });
+            break;    
+            case "about": 
+            break; 
+
+            default:
+            break;
+
+        }
         console.log("App started!");
         console.log("--------------------");
         console.log(`%cTitle: ${title}`,`font-size:15px; color: red;`);
+           
     }
     window.onload=start();
 })()
