6c4bf32a6558dd7442997e0db1b5690f2c1e4131
Khalil Frough <khalilfrough@Khalils-MBP.gateway>
2019-01-06 22:52:17 +1100

    added some codes to app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,32 +1,54 @@
 //this is a custom JavaScript file
 /* JavaScript has a function called IIFE => Immediately Invocked Function Expression */
 
-(function(){
-    // this is used to prevent JavaScript to automatically make variables 
-    "use strict"
+(function() {
+  // this is used to prevent JavaScript to automatically make variables
+  "use strict";
 
-    function start(){
+  function buttonClicked() {
+    console.log("About button clicked");
+  }
+  function aboutButtonOver(event) {
+    event.currentTarget.style.opacity = 0.1;
+  }
+  function aboutButtonOut(event) {
+    event.currentTarget.style.opacity = 0.5;
+  }
 
-        let title=document.title;
+  function homeContent() {
+    let AboutButton = document.getElementById("AboutButton");
+    AboutButton.textContent = "About JavaScript";
+    AboutButton.addEventListener("click", buttonClicked);
+    AboutButton.addEventListener("mouseover", aboutButtonOver);
+    AboutButton.addEventListener("mouseout", aboutButtonOut);
+  }
 
-        switch(title){
-            case "Home":
-                let AboutButton=document.getElementById("AboutButton");
-                AboutButton.addEventListener("click",function(){
-                console.log("About Button was clicked!");
-                });
-            break;    
-            case "about": 
-            break; 
+  function returnPi(){
+      return Math.PI;
+  }
+  function aboutContent(){
+      let myNumber=returnPi();
+      console.log("the Value of PI is: "+myNumber);
+      let para= document.getElementById("para");
+      para.textContent="this is about JavaScript learning course!"
+  }
 
-            default:
-            break;
+  function start() {
+    let title = document.title;
+    console.log("App started!");
+    console.log("--------------------");
+    console.log(`%cTitle: ${title}`, `font-size:15px; color: red;`);
+    switch (title) {
+      case "Home":
+        homeContent();
+        break;
+      case "About":
+        aboutContent();
+        break;
 
-        }
-        console.log("App started!");
-        console.log("--------------------");
-        console.log(`%cTitle: ${title}`,`font-size:15px; color: red;`);
-           
+      default:
+        break;
     }
-    window.onload=start();
-})()
+  }
+  window.addEventListener("load", start);
+})();
